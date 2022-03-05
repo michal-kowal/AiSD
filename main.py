7e5ac63f18a577591fef5a02eefeec11c1aa0c78
michal-kowal <kowalewski.michal777@gmail.com>
2022-03-05 12:59:32 +0100

    Dodano generator losowej tablicy oraz insertion sort

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
 import random
 import time
 
-# generowanie danych
+# generatory danych
+
 # losowe liczby
 def losowe(minimum, maximum, length):
     tab = []
     for i in range(length):
         tab.append(random.randint(minimum, maximum))
     return tab
+
+
+# tablica rosnaca
+def rosnaca(length):
+    tab = []
+    for i in range(0, length):
+        tab.append(i)
+    return tab
+
+
+#tablica malejaca
+def malejaca(length):
+    tab = []
+    for i in range(length - 1, -1, -1):
+        tab.append(i)
+    return tab
+
+
+#tablica A-ksztaltna
+def a_ksztaltna(length):
+    tab = []
+    for i in range(0, int(length / 2)):
+        tab.append(2 * i + 1)
+    for i in range(int(length / 2), length):
+        tab.append(2 * (length - i))
+    return tab
+
+
+#tablica V-ksztaltna
+def v_ksztaltna(length):
+    tab = []
+    for i in range(0, int(length / 2)):
+        tab.append(2 * (int(length / 2) - i))
+    for i in range(int(length / 2), length):
+        tab.append(2 * (i - int(length / 2)) + 1)
+    return tab
+
+
 
 # insertion sort
 def insertion_sort(array):
@@ -33,4 +72,9 @@ def insertion_sort(array):
     return array
 
 
+print(losowe(100, 1000, 10))
+print(rosnaca(10))
+print(malejaca(10))
+print(a_ksztaltna(8))
+print(v_ksztaltna(11))
 insertion_sort([2,9,3])
