2a0188edfce970d3d39847a22672aa6a26fa866d
michal-kowal <kowalewski.michal777@gmail.com>
2022-03-27 17:40:00 +0200

    Początek prac Dodano: -początek menu -walidację wprowadzanych danych -funkcje konstruującą losowe drzewo BST -szukanie elementu max i min

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 
 def bst(elementy):
@@ -24,6 +25,48 @@ def bst(elementy):
                 else:
                     rodzic = drzewo[rodzic][3]
     return drzewo
+
+
+def minimalny(tab):
+    start = time.time()
+    mini = tab[0][0]
+    path = []
+    index = 0
+    path.append(mini)
+    while True:
+        if tab[index][2] is not None:
+            index = tab[index][2]
+            mini = tab[index][0]
+            path.append(mini)
+        else:
+            break
+    stop = time.time()
+    print("============================================")
+    print("Element minimalny: ", mini)
+    print("Sciezka poszukiwania: ", *path)
+    print("Czas operacji: ", stop-start)
+    print("============================================")
+
+
+def maksymalny(tab):
+    start = time.time()
+    maxi = tab[0][0]
+    path = []
+    index = 0
+    path.append(maxi)
+    while True:
+        if tab[index][3] is not None:
+            index = tab[index][3]
+            maxi = tab[index][0]
+            path.append(maxi)
+        else:
+            break
+    stop = time.time()
+    print("============================================")
+    print("Element maksymalny: ", maxi)
+    print("Sciezka poszukiwania: ", *path)
+    print("Czas operacji: ", stop - start)
+    print("============================================")
 
 
 def menu():
@@ -53,13 +96,38 @@ def menu():
     while powtorz:
         print("[1] Skonstruuj drzewo AVL \n[2] Skonstruuj losowe drzewo BST")
         typ = int(input())
+        print()
+        print("Dane wejsciowe: ",*tab)
+        print("format drzewa: {węzeł: [klucz, rodzic, lewy potomek, prawy potomek]}")
         if typ == 1:
-            #avl(tab)
+            # drzewo = avl(tab)
             powtorz = False
         elif typ == 2:
-            print(bst(tab))
+            drzewo = bst(tab)
             powtorz = False
         else:
             print("Wybierz poprawna opcje")
+        print(drzewo)
+        print()
+    powtorz = True
+    while powtorz:
+        procedura = input("[1] Wyszukaj najmniejszy element w drzewie\n"
+                          "[2] Wyszukaj największy element w drzewie\n"
+                          "[3] Usuń element o wybranej wartości klucza\n"
+                          "[4] Wypisz wszystkie elementy drzewa w porządku rosnącym (in-order)\n"
+                          "[5] Wypisz wszystkie elementy drzewa w porządku pre-order\n"
+                          "[6] Usuń drzewo element po elemencie (post-order)\n"
+                          "[7] Wypisz w porządku pre-order poddrzewa o danym kluczu\n"
+                          "[8] Równoważ drzewo przez rotacje (DSW) lub przez usuwanie korzenia\n"
+                          "[9] Wyjście\n")
+        if procedura == "1":
+            minimalny(drzewo)
+        elif procedura == "2":
+            maksymalny(drzewo)
+        elif procedura == "9":
+            powtorz = False
+        else:
+            print("Wybierz poprawna opcje")
+
 
 menu()
